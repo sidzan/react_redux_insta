27e2dd3e05174abccbe7b1ab6261c6cc3e541adc
sijan shrestha <sijanshrestha2@gmail.com>
2016-07-05 23:16:40 +0700

    FIX

---
+++ client/data/post.js
@@ -0,0 +1,41 @@
+const posts = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"No Captions yest",
+      "likes":56,
+      "id":"1161022966406956503",
+      "display_src":"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/13529062_1422375521126467_314939878173054712_n.jpg?oh=f511506c6af588e5a71f91b7cee78580&oe=57FE9BA7"
+   },
+   {
+      "code":"BAcJeJrQca9",
+      "caption":"No Captions yest",
+      "likes":59,
+      "id":"1160844458347054781",
+      "display_src":"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/13516498_1421726191191400_7825924413026223044_n.jpg?oh=27074aeff22ea0ace9e8517c44d2153f&oe=57FFCE34"
+   },
+   {
+      "code":"BAF_KY4wcRY",
+      "caption":"No Captions yest",
+      "likes":79,
+      "id":"1154606670337393752",
+      "display_src":"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/13339694_1403373729693313_6129605370962620635_n.jpg?oh=79147da9a06d3f140d0a46494dc7fbe1&oe=5801A492"
+   },
+   {
+      "code":"BAPIPRjQce9",
+      "caption":"No Captions yest",
+      "likes":47,
+      "id":"1157179863266871229",
+      "display_src":"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/13133266_1385324434831576_1852721214287760460_n.jpg?oh=aa3b23f40afb5361b7e61c5dddb36802&oe=582E7E77"
+   },
+   {
+      "code":"-B3eiIwcYV",
+      "caption":"No Captions yest",
+      "likes":33,
+      "id":"1117418173361145365",
+      "display_src":"https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/12791084_1341584659205554_2849221414848473755_n.jpg?oh=0fd0b79e6e0d44901c746a16560ec72e&oe=57FD262A"
+   },
+];
+
+
+export default posts;
+
