e8c33b53ef2cded7bd5a82fc302a29849dbf5544
sijan shrestha <sijanshrestha2@gmail.com>
2016-07-05 19:22:12 +0700

    Fix

---
+++ client/components/comment.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Comments = React.createClass({
+
+	render: function() {
+		return (
+			<div >
+				this is a comment
+			</div>
+		);
+	}
+
+});
+
+module.exports = Comments;
